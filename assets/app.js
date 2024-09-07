4a64f76a729a2b7c73bfdb03a56dd0393f62a0bb
Brandon Charnick <37631159+brandon-charnick@users.noreply.github.com>
2024-09-07 00:14:53 -0700

    Refactor index template and remove unused JavaScript code

--- assets/app.js
+++ assets/app.js
@@ -18,7 +18,6 @@ require('bootstrap');
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
-
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
+// $(document).ready(function() {
+//     $('[data-toggle="popover"]').popover();
+// });
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -14,7 +14,7 @@ class IndexController extends AbstractController
     public function index(): Response
     {
         return $this->render(
-            'index/index.html.twig'
+            'index.html.twig'
         );
     }
 }
